c2926cff86c0a23725d7eee9ad7c662bf35330b4
Logeshwaran <141913551+logeshsalem@users.noreply.github.com>
2025-11-11 18:36:26 +0530

    Update auditors.sql

--- auditors.sql
+++ auditors.sql
@@ -13,7 +13,7 @@ select * from auditors;
 drop table auditors;
 
 create table store(
-id bigserial primary key,
+store_id bigserial primary key,
 name varchar(255) not null,
 address text,
 location_lat double precision not null,
@@ -23,6 +23,18 @@ store_status varchar(20) not null
 
 select * from store;
 
+drop table store;
+
+CREATE TABLE audit_plan (
+    audit_id BIGSERIAL PRIMARY KEY,    
+    auditor_id BIGINT REFERENCES auditors(auditor_id) ON DELETE SET NULL,
+    store_id BIGINT REFERENCES store(store_id) ON DELETE CASCADE,   
+    audit_priority VARCHAR(20) NOT NULL, -- High / Medium / Low  
+    audit_status VARCHAR(20) NOT NULL 
+);
+
 select * from audit_plan;
+
+drop table audit_plan;
 
 
